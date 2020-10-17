96d76e58933028345cd004c7b7d69fbfc0fc24ae
jfkquality <jfkquality@gmail.com>
2020-10-16 18:49:58 -0700

    633. Task 0. Bugfix. Lots of linter errors.

--- utils/redis.js
+++ utils/redis.js
@@ -1,30 +1,29 @@
 import redis from 'redis';
-const client = redis.createClient();
+
 const { promisify } = require('util');
-const getAsync = promisify(client.get).bind(client);
 
 class RedisClient {
-    constructor() {
-	client.on('error', function(err) {
-	    console.log(err);
-	});
-    }
-    isAlive() {
-	client.on('error', function(err) {
-	    return (false);
-	})
-	client.on('connect', function() {
-	    return (true);
-	})
-    }
-    async set(key, val, duration) {
-	client.set(await getAsync(key, val, duration * 1000));
-    }
-    async get(key) {
-	client.get(key, function(err, res) {
-	    return (res);
-	});
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    this.client.on('error', () => false);
+    this.client.on('connect', () => true);
+  }
+
+  async set(key, val, duration) {
+    this.client.set(await this.getAsync(key, val, duration * 1000));
+  }
+
+  async get(key) {
+    this.client.get(await this.getAsync(key, (err, res) => res));
+  }
 }
 
 const redisClient = new RedisClient();
