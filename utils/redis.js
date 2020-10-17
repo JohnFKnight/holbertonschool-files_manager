4157488ab6783176339bc1c310d17e96583a03bc
jfkquality <jfkquality@gmail.com>
2020-10-16 23:28:51 -0700

    633. Task 0. Bugfix. Fix async set, get. Dont use client.get AND set/getAsync!

--- utils/redis.js
+++ utils/redis.js
@@ -6,6 +6,8 @@ class RedisClient {
   constructor() {
     this.client = redis.createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
 
     this.client.on('error', (err) => {
       console.log(err);
@@ -13,18 +15,25 @@ class RedisClient {
   }
 
   isAlive() {
-    this.client.on('error', () => false);
-    this.client.on('connect', () => true);
+    this.client.on('error', () => console.log('false'));
+    this.client.on('connect', () => console.log('true'));
+    // return (this.client.connected);
   }
 
   async set(key, val, duration) {
-    this.client.set(await this.getAsync(key, val, duration * 1000));
+    await this.setAsync(key, val);
+    this.client.expire(key, duration);
   }
 
   async get(key) {
-    this.client.get(await this.getAsync(key, (err, res) => res));
+    console.log(await this.getAsync(key)); // , (err, res) => console.log(res)));
+  }
+
+  async del(key) {
+    await this.delAsync(key, (err, res) => res);
   }
 }
 
 const redisClient = new RedisClient();
-export default redisClient;
+// export default redisClient;
+module.exports = redisClient;
